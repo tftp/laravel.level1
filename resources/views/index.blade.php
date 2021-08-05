4690e46b87f9a34b9f1507ac30f237e23b0c26b4
OS <goloborodko@inbox.ru>
2021-08-05 16:01:56 +0300

    add template and his splitting

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+
+@section('title', 'Laravel.level1')
+
+@section('content')
+
+    <div class="col-md-8 blog-main">
+
+        <div class="blog-post">
+            <h2 class="blog-post-title">Главная</h2>
+            <p>Информация о нас или обо мне.</p>
+        </div><!-- /.blog-post -->
+
+    </div><!-- /.blog-main -->
+
+@endsection
