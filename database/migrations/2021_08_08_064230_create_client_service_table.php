6b019a6b59e2c4f6189cde8bdb6846575e8c92c4
Oleg <goloborodko@inbox.ru>
2021-08-08 16:30:21 +0300

    add models Service, Client and seeders for this

---
+++ database/migrations/2021_08_08_064230_create_client_service_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientServiceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_service', function (Blueprint $table) {
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->foreignId('service_id')->constrained()->onDelete('cascade');
+            $table->primary(['client_id', 'service_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_service');
+    }
+}
---
+++ database/seeders/ServiceSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Service;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Service::create([
+            'name' => 'Услуга 1',
+        ]);
+    }
+}
